51dbb49f5f541935708c8370bfb9caeecfe1d2ef
swwind <swwind233@gmail.com>
2021-08-06 21:35:09 +0800

    update release bash

--- release.sh
+++ release.sh
@@ -1,34 +1,92 @@
+set -e
 
-if [ "$1" = "" ]; then
-  echo Please enter release version
-  exit 1
+if [ "$2" = "" ]; then
+  VERSION="debug"
 else
-  echo Start release $1.
+  VERSION="$2"
 fi
 
-if [ -d build ]; then
-  rm -rf build/
+if [ ! -d release ]; then
+  mkdir release
 fi
 
-mkdir build
+clear_build() {
+  if [ -d build ]; then
+    rm -rf build
+  fi
+  mkdir build
+}
 
-# build linux version
-cargo build --release
-cp target/release/upstream build/upstream
+# not work on my machine
+release_aarch64_linux() {
+  clear_build
 
-# build windows version
-cargo build --release --target x86_64-pc-windows-gnu
-cp target/x86_64-pc-windows-gnu/release/upstream.exe build/upstream.exe
+  cargo build --release --target aarch64-unknown-linux-gnu
 
-# copy assets
-cp -r assets build/assets/
+  cp target/release/upstream build/upstream
+  cp -r assets build/assets
 
-# copy DLLs
-cp $(find /usr/x86_64-w64-mingw32/bin/*.dll) build
+  cd build
 
-cd build
+  zip -r upstream-$VERSION-linux-aarch64.zip upstream assets
 
-zip -r upstream-$1-linux-x86_64.zip upstream assets
-zip -r upstream-$1-windows-x86_64.zip upstream.exe assets $(find *.dll)
+  cd ..
 
-rm -rf upstream upstream.exe assets $(find *.dll)
+  mv build/upstream-$VERSION-linux-aarch64.zip release
+  ls -alh release/upstream-$VERSION-linux-aarch64.zip
+}
+
+release_x86_64_linux() {
+  clear_build
+
+  cargo build --release
+
+  cp target/release/upstream build/upstream
+  cp -r assets build/assets
+
+  cd build
+
+  zip -r upstream-$VERSION-linux-x86_64.zip upstream assets
+
+  cd ..
+
+  mv build/upstream-$VERSION-linux-x86_64.zip release
+  ls -alh release/upstream-$VERSION-linux-x86_64.zip
+}
+
+release_x86_64_windows() {
+  clear_build
+
+  cargo build --release --target x86_64-pc-windows-gnu
+
+  cp target/x86_64-pc-windows-gnu/release/upstream.exe build/upstream.exe
+  cp -r assets build/assets
+  cp $(find /usr/x86_64-w64-mingw32/bin/*.dll) build
+
+  cd build
+
+  zip -r upstream-$VERSION-windows-x86_64.zip upstream.exe assets $(find *.dll)
+
+  cd ..
+
+  mv build/upstream-$VERSION-windows-x86_64.zip release
+  ls -alh release/upstream-$VERSION-windows-x86_64.zip
+}
+
+case "$1" in
+  "all")
+    release_x86_64_linux
+    release_x86_64_windows
+    ;;
+  "linux")
+    release_x86_64_linux
+    ;;
+  "windows")
+    release_x86_64_windows
+    ;;
+  *)
+    echo "Unknown platform \`$1\`, which should be one of the following:"
+    echo "all, linux, windows"
+    exit 1
+    ;;
+esac
